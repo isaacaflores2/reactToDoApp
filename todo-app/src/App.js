2ec5ccb650c80679d8c7e47eebfea77d06a4c29e
isaacaflores2 <isaac.a.flores2@gmail.com>
2020-03-21 14:23:57 -0700

    Add abilityt to add new TodoList to app component

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,15 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import TodoList from './TodoList'
 
-function App ({items}){
+class App extends React.Component{
+
+  constructor(props){
+    super(props);
+    this.handleTodoNameChange = this.handleTodoNameChange.bind(this);
+    this.handleNewTodoList = this.handleNewTodoList.bind(this);
+    this.state = {todos: [], newTodoName: ''};
+  }
+
+  handleTodoNameChange(event){
+    this.setState({newTodoName: event.target.value});
+  }
+
+  handleNewTodoList(event){
+    event.preventDefault();
+    const newTodoListName = this.state.newTodoName;
+    this.setState(state => ({
+      todos: this.state.todos.concat(newTodoListName),
+      newTodoName: ''
+    }));
+  }
+
+  render(){
+    const todos = this.state.todos;
     return (
       <div className="App">
-        <TodoList name="My Chores" items={items}/>
+        {todos.map(todoName => 
+          <TodoList key={todoName} name={todoName}/>
+        )}
+
+        <form onSubmit={this.handleNewTodoList}>
+          <label htmlFor="newTodoList"/>
+          <input id="newTodoList" onChange={this.handleTodoNameChange} value={this.state.newTodoName}/>
+          <button>Add list</button>
+        </form>        
       </div>
       
     );
+  }
 }
 
 export default App;

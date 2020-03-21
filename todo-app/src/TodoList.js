52afc8d13b6f4957b3aee7b336a37119af9f67cc
isaacaflores2 <isaac.a.flores2@gmail.com>
2020-03-21 14:23:38 -0700

    Add ability to add new items to TodoList component

--- todo-app/src/TodoList.js
+++ todo-app/src/TodoList.js
@@ -1,20 +1,50 @@
 import React from 'react';
 import ToDoItem from "./ToDoItem";
 
-function Todolist ({name, items}){
-    return(
-        <div>
-            <header>
-                <h1>{name}</h1>
-            </header>
+class TodoList extends React.Component{
 
-            {items.map((item,i) => 
-                    <ToDoItem name={item} id={i}/>
-                
-            )}  
+    constructor(props){
+        super(props);
+        this.handleTextChange = this.handleTextChange.bind(this);        
+        this.handleNewItem = this.handleNewItem.bind(this);
+        this.state = {items: [], itemText: ''};
+    }
 
-        </div>
-    );
+    handleTextChange(event){
+        this.setState({itemText: event.target.value});
+    }
+
+    handleNewItem(event){
+        event.preventDefault();
+
+        const newItem = this.state.itemText;
+        this.setState( state => ({
+            items: state.items.concat(newItem),
+            itemText: ''
+        }) );
+    }
+
+
+    render(){
+        const items = this.state.items;
+        return(           
+            <div>
+                <header>
+                    <h1>{this.props.name}</h1>
+                </header>
+    
+                {items.map((item,i) => 
+                        <ToDoItem key={item} name={item} id={i}/>                
+                )}
+                <form onSubmit={this.handleNewItem}>
+                    <label htmlFor="newItem"/>
+                    <input id="newItem" onChange={this.handleTextChange} value={this.state.itemText}/>
+                    <button>Add</button>
+                </form>  
+    
+            </div>
+        )
+    }
 }
 
-export default Todolist;
+export default TodoList;

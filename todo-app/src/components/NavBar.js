2aaf4690d644e3608bee21859a7dc7c7ec0d5bee
Isaac Flores <isflores@microsoft.com>
2020-03-23 09:34:17 -0700

    Add App test

--- todo-app/src/components/NavBar.js
+++ todo-app/src/components/NavBar.js
@@ -3,8 +3,8 @@ import React from 'react';
 class NavBar extends React.Component{
     render(){
         return(
-            <nav class="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-                <a class="navbar-brand col-sm-3 col-md-2 mr-0" >To Do</a>
+            <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+                <a className="navbar-brand col-sm-3 col-md-2 mr-0" >To Do</a>
             </nav>
         )
     };

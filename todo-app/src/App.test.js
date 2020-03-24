e89c18d266b5e2d25f4d2ac3dc8156212ad5e70e
Isaac Flores <isflores@microsoft.com>
2020-03-24 09:50:44 -0700

    Add react-testing-library and test

--- todo-app/src/App.test.js
+++ todo-app/src/App.test.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { render } from '@testing-library/react';
+// react-testing-library renders your components to document.body,
+// this adds jest-dom's custom assertions
+import '@testing-library/jest-dom/extend-expect';
 
-it('renders withou crashing', () =>{
+it('renders without crashing', () =>{
     const div = document.createElement('div');
     ReactDOM.render(<App/>, div);
+});
+
+it('renders "My Lists"', () => {
+    const { getByText } = render(<App />);
+    expect(getByText('My Lists')).toBeInTheDocument();
 });
